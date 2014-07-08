d8862e8b8c9877105f4b72422bc20280a614c648
uobikiemukot <mxs@m2.gyao.ne.jp>
2014-07-08 23:55:44 +0900

    remove -O3 flag

--- jni/Android.mk
+++ jni/Android.mk
@@ -18,7 +18,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE    := yaft
 LOCAL_SRC_FILES := yaft.c
-LOCAL_CFLAGS    := -Wall -Wextra -O3
+LOCAL_CFLAGS    := -Wall -Wextra
 LOCAL_LDLIBS    := -llog -landroid
 LOCAL_STATIC_LIBRARIES := android_native_app_glue
 
